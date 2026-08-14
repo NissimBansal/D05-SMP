80efd59fb517a71e7ac775a83925e4147d40c2e2
NissimBansal <aryan.nissim@gmail.com>
2026-08-14 18:28:16 +0530

    Updated fullAdder.v

--- Testing/fullAdder.v
+++ Testing/fullAdder.v
@@ -1,10 +1,11 @@
-module fullAdder (   input a,
-                        input b,
-                        input cin,
-                        output out,
-                        output cout)
+module fullAdder (  input a,
+                    input b,
+                    input cin,
+                    output out,
+                    output cout)
 ;
 
 assign out = a ^ b ^ cin;
 assign cout = ( a & b ) | ( a & cin ) | ( b & cin );
+
 endmodule
