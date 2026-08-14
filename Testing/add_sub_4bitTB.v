a00ee7a3a316ae141277d67bb2cae7706d99ff0b
NissimBansal <aryan.nissim@gmail.com>
2026-08-14 19:23:13 +0530

    Updated 4bitadd/sub

--- Testing/add_sub_4bitTB.v
+++ Testing/add_sub_4bitTB.v
@@ -1,3 +1,36 @@
-module add_sub_4bitTB
+`timescale 1ns / 1ps
+module add_sub_4bitTB ();
+
+reg signed [3:0] a;
+reg signed [3:0] b;
+reg signed [3:0] bn;
+reg cin;
+reg SUB;
+reg SUB_4bit;
+wire signed [3:0] out;
+wire cout;
+wire overflow;
+
+wire c1, c2, c3;
+
+always @(SUB) begin
+     cin = SUB;
+     SUB_4bit = {4{SUB}};
+     bn = ( b ^ SUB_4bit ) + ( 1 & SUB );
+end
+
+fullAdder fa1(.a(a[0]), .b(bn[0]), .c_in(cin), .out(out[0]), .cout(c1));
+fullAdder fa2(.a(a[1]), .b(bn[1]), .c_in(c1), .out(out[1]), .cout(c2));
+fullAdder fa3(.a(a[2]), .b(bn[2]), .c_in(c2), .out(out[2]), .cout(c3));
+fullAdder fa4(.a(a[3]), .b(bn[3]), .c_in(c3), .out(out[3]), .cout(cout));
+
+assign overflow = c3 ^ cout;
+
+
+
+initial begin
+    $dumpfile ("add_sub_4bit.vcd");
+    $dumpvars (0,add_sub_4bitTB);
+end
 
 endmodule
