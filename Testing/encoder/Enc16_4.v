be9ddb14aa57db7dec79c05d582a86eb67d7ce06
NissimBansal <aryan.nissim@gmail.com>
2026-08-30 17:24:57 +0530

    Added encoder module

--- Testing/encoder/Enc16_4.v
+++ Testing/encoder/Enc16_4.v
@@ -2,6 +2,16 @@ module enc16_4 (input [15:0] KEY,
                 output [3:0] CODE)
 ;
 
+initial begin
 
+CODE <= 4'b0000; 
+
+while (KEY != 0) begin
+    
+    KEY <= (KEY >> 1);
+    CODE <= CODE + 1;
+end
+
+end
 
 endmodule
