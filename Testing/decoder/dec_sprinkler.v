d29142b422419a72a88139173d8ed2c7cdef8322
NissimBansal <aryan.nissim@gmail.com>
2026-08-31 16:23:45 +0530

    Completed sprinkler files

---
+++ Testing/decoder/dec_sprinkler.v
@@ -0,0 +1,47 @@
+module dec_if2_4(input [1:0] in,
+                 input en,
+                 output reg [3:0] out)
+;
+
+always @ (*) begin
+if (!en) begin
+
+out = 4'b0000;
+
+end else begin
+
+if ( in == 2'b00 ) out = 4'b0001;
+else if ( in == 2'b01 ) out = 4'b0010;
+else if ( in == 2'b10 ) out = 4'b0100;
+else if ( in == 2'b11 ) out = 4'b1000;
+
+end
+end
+endmodule
+
+module dec3_8 ( input [2:0] in,
+                output [7:0] out)
+;
+
+wire enn;
+
+assign enn = ~in[2];
+
+dec_if2_4 u0 (.in(in[1:0]), .en(enn), .out(out[3:0]));
+dec_if2_4 u1 (.in(in[1:0]), .en(in[2]), .out(out[7:4]));
+
+endmodule
+
+module dec_sprinkler (  input S, M, T, H,
+                        output S1, S2)
+;
+
+wire [7:0] out;
+
+dec3_8 d0 (.in({S,M,T}), .out(out));
+
+assign S1 = out[5] | (out[7] & H);
+assign S2 = out[0] | out[1];
+
+
+endmodule
--- Testing/decoder/dec_sprinkler_tb.v
+++ Testing/decoder/dec_sprinkler_tb.v
@@ -1,6 +1,25 @@
 `timescale 1 ns / 1 ps
 module dec_sprinkler_tb ();
 
+reg S, M, T, H;
+wire S1, S2;
+integer i;
 
+dec_sprinkler d1 (.S(S), .M(M), .T(T), .H(H), .S1(S1), .S2(S2));
+
+initial begin 
+    {S, M, T, H} <= 4'b0110;
+    
+    $monitor("S=%b M=%b T=%b H=%b S1=%b S2=%b", S, M, T, H, S1, S2);
+
+    for (i = 0; i < 16; i = i + 1) begin
+       #10 {S, M, T, H} <= i;
+    end
+end
+
+initial begin
+    $dumpfile ("dec_sprinkler.vcd");
+    $dumpvars (0,dec_sprinkler_tb);
+end
 
 endmodule
