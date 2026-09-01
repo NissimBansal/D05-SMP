2aa4cd32e524f20bad41ee7d95cd221dd03acfc5
NissimBansal <aryan.nissim@gmail.com>
2026-09-01 07:49:18 +0530

    Modified Enc16_4.v

--- Testing/encoder/Enc16_4.v
+++ Testing/encoder/Enc16_4.v
@@ -13,11 +13,6 @@ while (i != 0) begin
         i = (i >> 1);
         count = count + 1;
     end
-
-// for (i = 0; i < 16; i = i + 1) begin
-//         if (KEY[i]) CODE = i[3:0];
-//     end
-
 end
 
 assign CODE = KEY ? count : 4'bzzzz;
